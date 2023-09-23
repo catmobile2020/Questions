5077fe95de90dfbb955a975312d1fa0b7cb85556
MMahmoudd <mahmoud.mostafa1008@gmail.com>
2023-09-23 17:19:13 +0300

    add register

--- components/landingPage/landingPage.vue
+++ components/landingPage/landingPage.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="landingPage">
     <div class="btn-container">
-      <v-row class="">
+      <v-row>
         <v-col cols="12" md="12" class="">
           <nuxt-link to="/login" class="link">
-            <v-btn class="start-btn"> Start </v-btn>
+            <v-btn class="start-btn primary" x-large> Login </v-btn>
+          </nuxt-link>
+          <nuxt-link to="/register" class="link">
+            <v-btn class="start-btn primary" x-large> Register </v-btn>
           </nuxt-link>
         </v-col>
       </v-row>
---
+++ components/register.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="conOfForm logIn">
+            <v-container class="loginForm">
+              <v-row class="d-flex justify-center">
+                  <v-col md="6">
+                    <v-text-field
+                      outlined
+                      placeholder="Name"
+                      required
+                      solo
+                      hide-details="auto"
+                    ></v-text-field>
+                  </v-col>
+  
+                  <v-col md="6">
+                    <v-text-field
+                      outlined
+                      class="formInput"
+                      placeholder="Age"
+                      type="number"
+                      required
+                      hide-details="auto"
+                      solo
+                    ></v-text-field>
+                  </v-col>
+                  <v-col md="6">
+                    <v-select
+                        :items="genders"
+                        label="Gender"
+                        outlined
+                        required
+                        hide-details="auto"
+                        solo
+                    ></v-select>
+                  </v-col>
+                  <v-col md="6">
+                    <v-text-field
+                      outlined
+                      class="formInput"
+                      placeholder="ID"
+                      type="number"
+                      required
+                      hide-details="auto"
+                      solo
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" v-for="(item, i) in questions" :key="i">
+                    <p>{{item}}</p>
+                    <v-radio-group
+                    mandatory
+                    row
+                    >
+                    <v-radio
+                        label="Yes"
+                        value="yes"
+                    ></v-radio>
+                    <v-radio
+                        label="No"
+                        value="No"
+                    ></v-radio>
+                    </v-radio-group>
+                  </v-col>
+                  <v-col cols="12" md="12" class="d-flex justify-center">
+                    <v-btn class="primary"> Start </v-btn>
+                  </v-col>
+              </v-row>
+            </v-container>
+      <v-snackbar
+        v-model="successSnackbar"
+        color="success"
+        shaped
+        absolute
+        bottom
+        right
+        :timeout="timeout"
+      >
+        {{ successMessage }}
+      </v-snackbar>
+      <v-snackbar
+        v-model="errorSnackbar"
+        color="red"
+        shaped
+        top
+        center
+        :timeout="timeout"
+      >
+        {{ errorMessage }}
+      </v-snackbar>
+    </div>
+  </template>
+  <script>
+  export default {
+    data() {
+      return {
+        showPassword: false,
+        email: "",
+        password: "",
+        Me: {},
+        successSnackbar: false,
+        errorSnackbar: false,
+        timeout: 3000,
+        successMessage: "",
+        errorMessage: "",
+        radios: null,
+        genders: [
+            'male',
+            'Female',
+        ],
+        questions: [
+            'Diabetic',
+            'Hypertensive',
+            'Dyslipidemic',
+            'Smoker',
+            'Family history of premature CAD'
+        ]
+      };
+    },
+    components: {},
+    methods: {},
+  };
+  </script>
+  
