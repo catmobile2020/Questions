1336ea95665da61a2ea6b7411f8b20e86f9ecdc4
MMahmoudd <mahmoud.mostafa1008@gmail.com>
2023-09-24 17:44:33 +0300

    add dashboard

---
+++ pages/dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="dashboard">
+      <section class="">
+        <dashboard />
+      </section>
+    </div>
+  </template>
+  
+  <script>
+  import dashboard from "../components/dashboard/dashboard.vue";
+  export default {
+    name: "landingPages",
+    components: {
+        dashboard,
+    },
+  };
+  </script>
+  
