9326bf64616c61e9f446f524588e1515eaab7236
hossam mahmoud <74540341+hossammahoud@users.noreply.github.com>
2023-10-04 13:24:10 +0300

    edit patient profile mob design

--- components/patient-profile.vue
+++ components/patient-profile.vue
@@ -21,119 +21,127 @@
               <v-col class="d-flex justify-center">
                 <img width="200" :src="userData.qr_code" alt="" />
               </v-col>
-              <div class="d-flex">
-                <p class="text-field-style">Name : {{ userData.name }}</p>
-                <p class="text-field-style">age : {{ userData.age }}</p>
-                <p class="text-field-style">gender : {{ userData.gender }}</p>
-                <p class="text-field-style">
-                  national id : {{ userData.national_id }}
-                </p>
-              </div>
+              <v-row class="d-flex">
+                <v-col md="3" cols="6">
+                  <p class="text-field-style">Name : {{ userData.name }}</p>
+                </v-col>
+                <v-col md="3" cols="6">
+                  <p class="text-field-style">age : {{ userData.age }}</p>
+                </v-col>
+                <v-col md="3" cols="6">
+                  <p class="text-field-style">gender : {{ userData.gender }}</p>
+                </v-col>
+                <v-col md="3" cols="6">
+                  <p class="text-field-style">
+                    national id : {{ userData.national_id }}
+                  </p>
+                </v-col>
+              </v-row>
             </div>
             <v-row class="d-flex">
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Family history of premature CAD :
                   {{ userData.family_history_of_premature_CAD }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Diabetic :
                   {{ userData.diabetic }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Hypertensive :
                   {{ userData.hypertensive }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Dyslipidemia :
                   {{ userData.dyslipidemia }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Smoker :
                   {{ userData.smoker }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Lipid Test :
                   {{ userData.lipid_test }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   hpa1c :
                   {{ userData.hpa1c }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Triglycerides :
                   {{ userData.triglycerides }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   HDL :
                   {{ userData.HDL }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   LDL :
                   {{ userData.LDL }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Total Cholesterol :
                   {{ userData.total_cholesterol }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Systolic Blood Pressure :
                   {{ userData.systolic_blood_pressure }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Diastolic Blood Pressure :
                   {{ userData.diastolic_blood_pressure }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Weight :
                   {{ userData.weight }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Height :
                   {{ userData.height }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Blood Sugar :
                   {{ userData.blood_sugar }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   Pulse :
                   {{ userData.pulse }}
                 </p>
               </v-col>
-              <v-col md="3" cols="4">
+              <v-col md="3" cols="6">
                 <p class="text-field-style">
                   10-year estimated risk for ASCVDs :
                   {{ userData.risk_assessment }}
